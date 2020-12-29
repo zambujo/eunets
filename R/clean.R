db6246106ef92d596d0415b616ea7e796f3ab871
Martins <zambuj@gmail.com>
2020-12-29 18:10:57 +0100

    ✨  Adding centrality table (See #4)
    - TODO: normalize by size

--- R/clean.R
+++ R/clean.R
@@ -1,4 +1,4 @@
-`%>%` <- magrittr::`%>%` # export the pipe operator
+source(here::here("R", "global.R"))
 
 # read --------------------------------------------------------------------
 
@@ -16,17 +16,6 @@ country_selection <- yaml::read_yaml(here::here("data", "countries.yml"))
 programme_parts <- yaml::read_yaml(here::here("data", "cordis-programmes.yml"))
 
 # logic -------------------------------------------------------------------
-
-list_to_df <- function(x, column_names) {
-  .build_tibble <- function(el, el_names) {
-    dplyr::tibble(el, el_names)
-  }
-  res <- purrr::map2_df(x, names(x), .build_tibble)
-  if (!missing(column_names)) {
-    names(res) = column_names
-  }
-  return(res)
-}
 
 info_country <- list_to_df(country_selection, c("country", "group"))
 info_programmes <- list_to_df(programme_parts, c("programme_slug", "programme_part"))
--- R/global.R
+++ R/global.R
@@ -1,1 +1,13 @@
+`%>%` <- magrittr::`%>%` # export the pipe operator
 
+
+list_to_df <- function(x, column_names) {
+  .build_tibble <- function(el, el_names) {
+    dplyr::tibble(el, el_names)
+  }
+  res <- purrr::map2_df(x, names(x), .build_tibble)
+  if (!missing(column_names)) {
+    names(res) = column_names
+  }
+  return(res)
+}
